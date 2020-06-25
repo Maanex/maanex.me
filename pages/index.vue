e4f025995ce92d4dee9203dcb95885a02216314e
maanex <maan91315@gmail.com>
2020-06-25 12:06:16 +0200

    <br>

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
       Hi I'm Maanex<br>
       and this website is still under construction :/<br>
       <br>
-      <a href="https://twitter.com/Maanex_">Twitter @Maanex_</a>
-      <a href="https://github.com/Maanex">Github @Maanex</a>
+      <a href="https://twitter.com/Maanex_">Twitter @Maanex_</a><br>
+      <a href="https://github.com/Maanex">Github @Maanex</a><br>
       <a href="https://discord.tude.ga/">Discord</a>
     </div>
   </div>
